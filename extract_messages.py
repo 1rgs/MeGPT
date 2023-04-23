260d6585ceea755c23752e6fbb3d399a13543551
rahulgs12 <gsr1998@gmail.com>
2023-04-23 12:00:28 -0400

    train on back and forths

--- extract_messages.py
+++ extract_messages.py
@@ -1,10 +1,11 @@
 import os
 import sqlite3
-import csv
+import json
 
 # Get the user's home directory
 home = os.path.expanduser("~")
 
+output_file = "response.json"
 # Path to the SQLite database
 db_path = f"{home}/Library/Messages/chat.db"
 
@@ -24,22 +25,55 @@ conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
 # Run the query
-query = "SELECT text FROM message WHERE is_from_me = '1' AND length(text) > 1;"
+query = """
+WITH messages_with_prev AS (
+    SELECT m.ROWID, m.guid, m.text, m.subject, m.country, m.date, chj.chat_id, m.is_from_me,
+           LAG(m.is_from_me) OVER (PARTITION BY chj.chat_id ORDER BY m.date) AS prev_is_from_me
+    FROM message AS m
+    JOIN chat_message_join AS chj ON m.ROWID = chj.message_id
+    WHERE LENGTH(m.text) > 0
+),
+grouped_messages AS (
+    SELECT *,
+           SUM(CASE WHEN is_from_me != IFNULL(prev_is_from_me, -1) THEN 1 ELSE 0 END) OVER (PARTITION BY chat_id ORDER BY date) AS grp
+    FROM messages_with_prev
+),
+consecutive_messages AS (
+    SELECT chat_id, is_from_me, group_concat(text, '\n') AS joined_text, MIN(date) AS min_date
+    FROM grouped_messages
+    GROUP BY chat_id, is_from_me, grp
+),
+my_consecutive_messages AS (
+    SELECT * FROM consecutive_messages WHERE is_from_me = 1
+),
+other_consecutive_messages AS (
+    SELECT * FROM consecutive_messages WHERE is_from_me = 0
+)
+
+SELECT other.joined_text AS prev_text, my.joined_text AS my_text
+        FROM my_consecutive_messages AS my
+LEFT JOIN other_consecutive_messages AS other ON my.chat_id = other.chat_id AND other.min_date < my.min_date
+WHERE other.min_date = (
+    SELECT MAX(min_date) FROM other_consecutive_messages AS ocm
+    WHERE ocm.chat_id = my.chat_id AND ocm.min_date < my.min_date
+)
+ORDER BY my.min_date;
+"""
 cursor.execute(query)
 
 # Fetch the results
 results = cursor.fetchall()
 
-# Save the response to a CSV file
-csv_file = "messages.csv"
-with open(csv_file, "w", newline="", encoding="utf-8") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["text"])
-    for row in results:
-        writer.writerow(row)
+print(results[0])
+# remove all newlines
+results = [tuple(map(lambda x: x.replace("\n", " "), row)) for row in results]
 
-# Close the SQLite connection
-cursor.close()
-conn.close()
 
-print(f"Results saved to '{csv_file}'")
+with open(output_file, "w") as f:
+    json.dump(
+        [{"text": f"person: {row[0]}\nMeGPT: {row[1]}", "label": 0} for row in results],
+        f,
+        indent=4,
+    )
+
+print(f"Saved {len(results)} messages to {output_file}")
--- generate.py
+++ generate.py
@@ -2,11 +2,14 @@ import torch
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
+    StoppingCriteria,
+    StoppingCriteriaList,
 )
 from peft import (
     PeftConfig,
     PeftModel,
 )
+import re
 
 model_name = "facebook/opt-1.3b-finetuned"
 config = PeftConfig.from_pretrained(model_name)
@@ -18,23 +21,61 @@ model = AutoModelForCausalLM.from_pretrained(
 
 tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 
-# Load the Lora model
 model = PeftModel.from_pretrained(model, model_name)
-
-batch = tokenizer(
-    "I really enjoyed the", return_tensors="pt", padding=True, truncation=True
-)
-
 model.to("cuda")
 
+stop_words_ids = [tokenizer.encode(stop_word) for stop_word in ["MeGPT:", "person:"]]
 
-with torch.cuda.amp.autocast():
-    output_tokens = model.generate(
-        **batch,
-        max_new_tokens=500,
-        temperature=0.9,
-        eos_token_id=tokenizer.eos_token_id,
-        pad_token_id=tokenizer.pad_token_id,
-    )
 
-print("\n\n", tokenizer.decode(output_tokens[0], skip_special_tokens=True))
+class StoppingCriteriaSub(StoppingCriteria):
+    def __init__(self, stops=[]):
+        StoppingCriteria.__init__(self),
+
+    def __call__(
+        self, input_ids: torch.LongTensor, scores: torch.FloatTensor, stops=[]
+    ):
+        self.stops = stops
+        for i in range(len(stops)):
+            self.stops = self.stops[i]
+
+
+stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
+
+
+def chat_with_model(input_text, conversation_history):
+    conversation_text = f"{conversation_history}person: {input_text}\nMeGPT: "
+
+    batch = tokenizer(conversation_text, return_tensors="pt", padding=True)
+    batch = {k: v.to("cuda") for k, v in batch.items()}
+
+    with torch.cuda.amp.autocast():
+        output_tokens = model.generate(
+            **batch,
+            max_new_tokens=100,
+            temperature=0.6,
+            pad_token_id=tokenizer.pad_token_id,
+            use_cache=True,
+            do_sample=True,
+            stopping_criteria=stopping_criteria,
+        )
+
+    response_text = tokenizer.decode(output_tokens[0], skip_special_tokens=True).strip()
+    if "  " in response_text:
+        regex = re.compile(r"\s{2,}")
+        split = regex.split(response_text)
+        response_text = split[1]
+
+    conversation_history += f"person: {input_text}\nMeGPT: {response_text}\n"
+
+    return response_text, conversation_history
+
+
+conversation_history = "MeGPT: hi i am you. How can I help?\n"
+print(conversation_history)
+while True:
+    user_input = input("> ")
+    if user_input.lower() == "quit":
+        break
+    response, conversation_history = chat_with_model(user_input, conversation_history)
+
+    print("MeGPT:", response)
